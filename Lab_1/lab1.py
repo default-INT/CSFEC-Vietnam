c5fefda64b8063c55d9780bfa68d50a9eacd0bf5
Evgeni Trofimov <int.tv.gs@gmail.com>
2019-10-20 17:41:03 +0300

    complete oart 1

--- Lab_1/lab1.py
+++ Lab_1/lab1.py
@@ -3,6 +3,8 @@ from tkinter import filedialog as fd
 from tkinter import ttk
 import tkinter as tk   
 from tkinter import messagebox
+import thermal_conductivity as tc
+import matplotlib.pyplot as plt
 
 
 class GUI(tk.Frame):
@@ -13,26 +15,105 @@ class GUI(tk.Frame):
     back_color = '#e9e8ea'
     text_color = 'black'
     font = 'Verdana 14'
+    width_entry = 6
 
     def __init__(self, root):
         super().__init__(root)
         self.main_window()
 
     def main_window(self):
-        create_button = tk.Button(root, text = 'Ввести данные', font = self.font, activebackground = self.active_color,
-                                  command = self.input_window, bg = self.button_color, fg = self.text_button,
-                                   activeforeground = self.active_color_text, bd=0, compound = tk.TOP)
-        create_button.grid(row = 0, column = 0, ipadx = 50, ipady=10 , padx = 5, pady = 5)
+        title_lable = tk.Label(root, text="Введите значения", bg = self.back_color,
+                                    fg = self.text_color, font = self.font)
+        title_lable.grid(row = 0, column = 1, padx = 5, pady = 5)
 
-        save_button = tk.Button(root, text = 'Сохранить данные', font = self.font, activebackground = self.active_color,
-                                  command = self.check, bg = self.button_color, fg = self.text_button,
-                                   activeforeground = self.active_color_text, bd=0, compound = tk.TOP)
-        save_button.grid(row = 0, column = 1, ipadx = 50, ipady=10 , padx = 5, pady = 5)
+        length_lable = tk.Label(root, text="Введите длину (м):", bg = self.back_color,
+                                fg = self.text_color, font = self.font)
+        length_lable.grid(row = 1, column = 0, padx = 5, pady = 5)
+        self.length_input = tk.Entry(root, width = self.width_entry, font = self.font, bg = 'white', fg = self.text_color)
+        self.length_input.grid(row = 1, column = 1, padx = 5, pady = 5)
 
-        mkr_cal_button = tk.Button(root, text = 'Вычислить МКР', font = self.font, activebackground = self.active_color,
-                                  command = self.input_window, bg = self.button_color, fg = self.text_button,
-                                   activeforeground = self.active_color_text, bd=0, compound = tk.TOP)
-        mkr_cal_button.grid(row = 0, column = 3, ipadx = 50, ipady=10 , padx = 5, pady = 5)
+        #input t_left
+        t_left_lable = tk.Label(root, text="Введите температуру на левом крае:", bg = self.back_color,
+                                fg = self.text_color, font = self.font)
+        t_left_lable.grid(row = 2, column = 0, padx = 5, pady = 5)
+        self.t_left_input = tk.Entry(root, width = self.width_entry, bg = 'white', font = self.font, fg = self.text_color)
+        self.t_left_input.grid(row = 2, column = 1, padx = 5, pady = 5)
+
+        #input t_right
+        t_right_lable = tk.Label(root, text="Введите температуру на правом крае:", bg = self.back_color,
+                                fg = self.text_color, font = self.font)
+        t_right_lable.grid(row = 3, column = 0, padx = 5, pady = 5)
+        self.t_right_input = tk.Entry(root, width = self.width_entry, font = self.font, bg = 'white', fg = self.text_color)
+        self.t_right_input.grid(row = 3, column = 1, padx = 5, pady = 5)
+
+        #input time
+        time_lable = tk.Label(root, text="Введите время:", bg = self.back_color,
+                                fg = self.text_color, font = self.font)
+        time_lable.grid(row = 4, column = 0, padx = 5, pady = 5)
+        self.time_input = tk.Entry(root, width = self.width_entry, font = self.font, bg = 'white', fg = self.text_color)
+        self.time_input.grid(row = 4, column = 1, padx = 5, pady = 5)
+
+        #input count_N
+        count_N_lable = tk.Label(root, text="Количество интервалов:", bg = self.back_color,
+                                fg = self.text_color, font = self.font)
+        count_N_lable.grid(row = 5, column = 0, padx = 5, pady = 5)
+        self.count_N_input = tk.Entry(root, width = self.width_entry, font = self.font, bg = 'white', fg = self.text_color)
+        self.count_N_input.grid(row = 5, column = 1, padx = 5, pady = 5)
+        
+        #input T0
+        start_t_lable = tk.Label(root, text="Введите начальную температуру:", bg = self.back_color,
+                                fg = self.text_color, font = self.font)
+        start_t_lable.grid(row = 6, column = 0, padx = 5, pady = 5)
+        self.start_t_input = tk.Entry(root, width = self.width_entry, font = self.font, bg = 'white', fg = self.text_color)
+        self.start_t_input.grid(row = 6, column = 1, padx = 5, pady = 5)
+        
+        #material combo
+        material_lable = tk.Label(root, text="Выберите материал:", bg = self.back_color,
+                                fg = self.text_color, font = self.font)
+        material_lable.grid(row = 7, column = 0, padx = 5, pady = 5)
+        self.material_combo = ttk.Combobox(root, font = self.font, width = self.width_entry)
+        self.material_combo['values'] = ('Гетинакс', 'Олово')
+        self.material_combo.grid(row = 7, column = 1, padx = 5, pady = 5)
+
+        fdm_cal_button = tk.Button(root, text = 'Вычислить по МКР', font = self.font, activebackground = self.active_color,
+                                command = lambda: self.fdm_cal(), bg = 'green', fg = 'white',
+                                activeforeground = self.active_color_text, bd=0, compound = tk.TOP)
+        fdm_cal_button.grid(row = 8, column = 0, ipadx = 50, ipady=10 , padx = 5, pady = 5)
+        
+        accept_button = tk.Button(root, text = 'Вычислить по МКЭ', font = self.font, activebackground = self.active_color,
+                                command = lambda: self.fdm_cal(), bg = self.button_color, fg = self.text_button,
+                                activeforeground = self.active_color_text, bd=0, compound = tk.TOP)
+        accept_button.grid(row = 8, column = 1, ipadx = 50, ipady=10 , padx = 5, pady = 5)
+
+    def choice_material(self, material):
+        if  material == 'Гетинакс':
+            return 1350, 1400
+        elif material == 'Медь':
+            return 8930, 385
+        else:
+            return 7800, 460
+
+    def fdm_cal(self):
+        try:
+            L = float(self.length_input.get())
+            T_left = int(self.t_left_input.get())
+            T_right = int(self.t_right_input.get())
+            T0 = int(self.start_t_input.get())
+            time = int(self.time_input.get())
+            N = int(self.count_N_input.get())
+            ro, c = self.choice_material(self.material_combo.get())
+            
+            thermal_conductivity = tc.ThermalConductivity(L=L, T_left=T_left, T_right=T_right, t=time,
+                                        N = N, T0=T0, Ro=ro, c=c)            
+            T, x = thermal_conductivity.FDM()
+
+            fig = plt.figure()
+            plt.plot(x, T)
+            plt.show()
+        except:
+            messagebox.showerror('Ошибка','Были некорректно  введены значения.')
+        #self.exit()
+        
 
     def check(self):
         pass
@@ -49,6 +130,7 @@ class GUI(tk.Frame):
         back_color = None
         text_color = None
         data = None
+        font = 'Verdana 14'
 
         def __init__(self, colors):
             super().__init__()
@@ -75,49 +157,49 @@ class GUI(tk.Frame):
             title_lable.grid(row = 0, column = 1, padx = 5, pady = 5)
 
             length_lable = tk.Label(self, text="Введите длину (м):", bg = self.back_color,
-                                    fg = self.text_color, font = "Arial 14")
+                                    fg = self.text_color, font = self.font)
             length_lable.grid(row = 1, column = 0, padx = 5, pady = 5)
-            self.length_input = tk.Entry(self, width = 30, bg = 'white', fg = self.text_color)
+            self.length_input = tk.Entry(self, width = self.width_entry, bg = 'white', fg = self.text_color)
             self.length_input.grid(row = 1, column = 1, padx = 5, pady = 5)
 
             #input t_left
             t_left_lable = tk.Label(self, text="Введите температуру на левом крае:", bg = self.back_color,
-                                    fg = self.text_color, font = "Arial 14")
+                                    fg = self.text_color, font = self.font)
             t_left_lable.grid(row = 2, column = 0, padx = 5, pady = 5)
-            self.t_left_input = tk.Entry(self, width = 30, bg = 'white', fg = self.text_color)
+            self.t_left_input = tk.Entry(self, width = self.width_entry, bg = 'white', fg = self.text_color)
             self.t_left_input.grid(row = 2, column = 1, padx = 5, pady = 5)
 
             #input t_right
             t_right_lable = tk.Label(self, text="Введите температуру на правом крае:", bg = self.back_color,
-                                    fg = self.text_color, font = "Arial 14")
+                                    fg = self.text_color, font = self.font)
             t_right_lable.grid(row = 3, column = 0, padx = 5, pady = 5)
-            self.t_right_input = tk.Entry(self, width = 30, bg = 'white', fg = self.text_color)
+            self.t_right_input = tk.Entry(self, width = self.width_entry, bg = 'white', fg = self.text_color)
             self.t_right_input.grid(row = 3, column = 1, padx = 5, pady = 5)
 
             #input time
             time_lable = tk.Label(self, text="Введите время:", bg = self.back_color,
-                                    fg = self.text_color, font = "Arial 14")
+                                    fg = self.text_color, font = self.font)
             time_lable.grid(row = 4, column = 0, padx = 5, pady = 5)
-            self.time_input = tk.Entry(self, width = 30, bg = 'white', fg = self.text_color)
+            self.time_input = tk.Entry(self, width = self.width_entry, bg = 'white', fg = self.text_color)
             self.time_input.grid(row = 4, column = 1, padx = 5, pady = 5)
 
             #input count_N
             count_N_lable = tk.Label(self, text="Количество интервалов:", bg = self.back_color,
-                                    fg = self.text_color, font = "Arial 14")
+                                    fg = self.text_color, font = self.font)
             count_N_lable.grid(row = 5, column = 0, padx = 5, pady = 5)
-            self.count_N_input = tk.Entry(self, width = 30, bg = 'white', fg = self.text_color)
+            self.count_N_input = tk.Entry(self, width = self.width_entry, bg = 'white', fg = self.text_color)
             self.count_N_input.grid(row = 5, column = 1, padx = 5, pady = 5)
             
             #input T0
             start_t_lable = tk.Label(self, text="Введите начальную температуру:", bg = self.back_color,
-                                    fg = self.text_color, font = "Arial 14")
+                                    fg = self.text_color, font = self.font)
             start_t_lable.grid(row = 6, column = 0, padx = 5, pady = 5)
-            self.start_t_input = tk.Entry(self, width = 30, bg = 'white', fg = self.text_color)
+            self.start_t_input = tk.Entry(self, width = self.width_entry, bg = 'white', fg = self.text_color)
             self.start_t_input.grid(row = 6, column = 1, padx = 5, pady = 5)
             
             #material combo
             material_lable = tk.Label(self, text="Выберите материал:", bg = self.back_color,
-                                    fg = self.text_color, font = "Arial 14")
+                                    fg = self.text_color, font = self.font)
             material_lable.grid(row = 7, column = 0, padx = 5, pady = 5)
             self.material_combo = ttk.Combobox(self)
             self.material_combo['values'] = ('Метал', 'Олово')
@@ -140,7 +222,7 @@ class GUI(tk.Frame):
 
 if __name__ == "__main__":
     root=tk.Tk()
-    root.geometry('800x420+140+90')
+    root.geometry('720x420+140+90')
     root.config(bg='#e9e8ea')
     root.title('Лабараторная работа №1. Современные численные методы решения граничных задач')
     GUI(root)
--- Lab_1/thermal_conductivity.py
+++ Lab_1/thermal_conductivity.py
@@ -1,10 +1,12 @@
-#import numpy
+import numpy as np
 import math
 
 class ThermalConductivity():
     
-    def __init__(self, L, lamba, Ro, c, T0, T_left, T_right, t):
+    def __init__(self, L = 0.1, T0 = 20, T_left = 300, T_right = 100, t = 15, Ro = 7800, N = 45,
+                       lamba = 46, c = 460):
         self.L = L
+        self.N = N
         self.lamba = lamba
         self.Ro = Ro
         self.c = c
@@ -26,24 +28,34 @@ class ThermalConductivity():
         return TT
 
     def FDM(self):
-        self.N = 45
-        self.t = 15
         a = self.lamba / (self.Ro * self.c)
         h = self.L / (self.N - 1)
         tau = h ** 2 / (4 * a)
         T0 = self.T0
         T = self.create_T()
-        T[0] = T_left
-        T[self.N-1] =T_right
+        T[0] = self.T_left
+        T[self.N-1] = self.T_right
         time = 0
         while time <= self.t:
             time += tau 
             TT = T
             for i in range(1, self.N - 1):
                 T[i] = TT[i] + a * tau / (h ** 2) * (TT[i+1] - 2 * TT[i] + TT[i-1])
-                    
-        return T
-    
+        k = 0
+        L_arr = list()
+        while (k < self.L):
+            L_arr.append(k)
+            k+=h
+        return T, L_arr
+
+    def FEM(self):
+        a = self.lamba / (self.Ro * self.c)
+        len_i = self.L / (self.N - 1) #длина i-го эл.
+        tau = len_i ** 2 / (4 * a) #шаг по времени
+        C_i = (self.c * self.Ro * math.pi * len_i) / 6 * np.array([[2, 1], [1, 2]], dtype='double')
+        K_i = (math.pi * self.lamba) / len_i * np.array([[1, -1], [-1, 1]], dtype='double')
+
+'''    
 L = 0.1
 lamba = 46
 Ro = 7800
@@ -56,3 +68,4 @@ t = 60
 thermCond = ThermalConductivity(L, lamba, Ro, c, T0, T_left, T_right, t)
 arr = thermCond.FDM()
 print(arr)
+'''
